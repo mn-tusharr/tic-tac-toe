edae3b41322b66c4d588910bb99cb7c3f8673cc1
Tushar Meena <25b1078@iitb.ac.in>
2026-02-02 18:54:07 +0530

    Initial Commit: Tic-Tac-Toe

---
+++ play.js
@@ -0,0 +1,95 @@
+const boxes = document.querySelectorAll(".box");
+const reset = document.querySelector("#reset");
+
+let idx = 0;
+
+const mark = ["x","o"];
+
+const winPattern = ["123","456","789","147","258","369","159","357"];
+
+let gameRunning = true;
+
+    //For alternating the X and O clicks 
+    // document.body.addEventListener("mouseover", ()=>{ //to rectify the double click problem during the first click
+    // })
+
+reset.addEventListener("click", ()=>{
+    boxes.forEach(element => {
+        element.setAttribute("class", "box");
+        element.innerHTML = null;
+        document.querySelector("#news").innerHTML = "";
+        gameRunning = true;
+        idx = 0;
+    });
+})
+
+
+for (const box of boxes) {
+    box.addEventListener("click", ()=>{
+        if(!gameRunning){
+            return;
+        }
+        if(!box.innerHTML){
+            box.innerHTML = `${mark[idx]}`;
+            box.classList.add(`${mark[idx]}-mark`);
+        
+            //Function will be called to check winning.
+            checkWin(idx);
+
+            idx = idx>0 ? 0 : 1;
+        }
+    })
+}
+
+
+let checkWin = (i) =>{
+    let text = "";
+    if(i==0){
+        let set = document.querySelectorAll(".x-mark");
+        set.forEach(element => {
+            text += element.getAttribute("id");
+        });
+        console.log(text);
+
+        //Logic - if the winning pattern matches in the text sequence, then 'X' wins
+        for (const pattern of winPattern) {
+            if(isSubsequence(pattern,text)){
+                document.querySelector("#news").innerHTML = "X won";
+                gameRunning = false;
+            }
+        }
+
+    }
+    else{
+        let set = document.querySelectorAll(".o-mark");
+        set.forEach(element => {
+            text += element.getAttribute("id");
+        });
+        console.log(text);
+
+        //Logic - if the winning pattern matches in the text sequence, then 'O' wins
+        for (const pattern of winPattern) {
+            if(isSubsequence(pattern,text)){
+                document.querySelector("#news").innerHTML = "O won";
+                gameRunning = false;
+            }
+        }
+    }
+}
+
+function isSubsequence(pattern, text) {
+  let i = 0; // pattern pointer
+  let j = 0; // text pointer
+
+  while (i < pattern.length && j < text.length) {
+    if (pattern[i] === text[j]) {
+      i++;
+    }
+    j++;
+  }
+
+  return i === pattern.length;
+}
+
+
+
